7410b07d8f706c3e51bde6777c6f1fae220820b5
TonTech-GH <idecchimctv+github@gmail.com>
2020-06-04 07:11:17 +0900

    プライオリティに応じたタスクの色変更

--- todo/models.py
+++ todo/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+PRIORITY = (('danger','high'), ('info','normal'), ('success','low'))
+
 class TodoModel(models.Model):
     # カラム名 = モデルフィールド
     title = models.CharField(max_length=100)
     memo  = models.TextField()
+    priority = models.CharField(max_length = 50, choices = PRIORITY)
+    duedate = models.DateField()
 
     def __str__(self):
         '''オブジェクトを文字列として返す特殊メソッド'''
