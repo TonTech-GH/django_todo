b17b8cac0204e6694b6202a928a1290b69933772
TonTech-GH <idecchimctv+github@gmail.com>
2020-06-05 06:23:03 +0900

    入力フォーム生成

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/', views.TodoList.as_view()),
-    path('detail/<int:pk>', views.TodoDetail.as_view()),
+    path('list/', views.TodoList.as_view(), name='list'),
+    path('detail/<int:pk>', views.TodoDetail.as_view(), name='detail'),
+    path('create/', views.TodoCreate.as_view(), name='create'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from . import models
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -12,3 +13,15 @@ class TodoDetail(DetailView):
     template_name = 'detail.html'
     model = models.TodoModel
 
+class TodoCreate(CreateView):
+    template_name = 'create.html'
+    model = models.TodoModel
+    
+    # 生成時の入力対象データを指定(form.as_pに対応)
+    fields = ('title', 'memo', 'priority', 'duedate')
+    
+    # データ作成成功時のURL指定
+    # path名を指定してそれに紐づくURLに遷移する
+    # class based viewで使う時はreverse_lazy()
+    # function based viewではreverse()
+    success_url = reverse_lazy('list')
